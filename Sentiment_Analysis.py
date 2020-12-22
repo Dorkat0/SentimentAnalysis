4372ee4a4dd40cc53f1f163f217aee3e62afedb7
Dorkat0 <14137285+Dorkat0@users.noreply.github.com>
2020-12-22 10:51:21 +0100

    setup_v1

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -1,8 +1,8 @@
+from datetime import datetime
 import os
 
 import import_ as imp
 from sentistrength import PySentiStr
-import pandas as pd
 
 se = PySentiStr()
 path = os.path.abspath("SentiStrength.jar")
@@ -13,12 +13,14 @@ statement = imp.importCSV("data/SOCC/raw/gnm_articles.csv")
 statement['sentimentResult_pos'] = 0
 statement['sentimentResult_neg'] = 0
 
-number_of_statements = statement.count()
+number_of_statements = statement['article_id'].count()
 
 # statement = statement.head(20)          # TODO comment for howl dataset
 
 print("stated...")
-print(number_of_statements + " statements")
+print(str(number_of_statements) + " statements")
+start_time = datetime.now()
+print("stated at: " + str(start_time))
 
 iteration = 0
 for e, row in statement.iterrows():
@@ -33,11 +35,14 @@ for e, row in statement.iterrows():
     # e[1]['sentimentResult'] = comment_sum
 
     iteration = iteration + 1
-    if iteration > 10000:
-        print("next 10.000")
+    if iteration > 5000:
+        print("next 5.000")
         iteration = 0
 
 print("statement done")
+end_time = datetime.now()
+print("ended at: " + str(end_time))
+print("duration: " + str((end_time - start_time)))
 
 comment = imp.importCSV("data/SOCC/raw/gnm_comments.csv")
 comment['sentimentResult_pos'] = 0
@@ -45,8 +50,10 @@ comment['sentimentResult_neg'] = 0
 
 # comment = comment.head(20)          # TODO comment for howl dataset
 
-number_of_comments = comment.count()
-print(number_of_comments + " statements")
+number_of_comments = comment['comment_id'].count()
+print(str(number_of_comments) + " comments")
+start_time = datetime.now()
+print("stated at: " + str(start_time))
 
 iteration = 0
 for e, row in comment.iterrows():
@@ -60,14 +67,20 @@ for e, row in comment.iterrows():
     # e[1]['sentimentResult'] = comment_sum
 
     iteration = iteration + 1
-    if iteration > 10000:
-        print("next 10.000")
+    if iteration > 5000:
+        print("next 5.000")
         iteration = 0
     # print('added comment')
 
 print("comments done")
+end_time = datetime.now()
+print("ended at: " + str(end_time))
+print("duration: " + str(end_time - end_time))
 
 print("create combination")
+start_time = datetime.now()
+print("stated at: " + str(start_time))
+
 iteration = 0
 statement['comments'] = None
 for e, row in statement.iterrows():
@@ -76,11 +89,14 @@ for e, row in statement.iterrows():
     statement.at[e, 'comments'] = comment[comment['article_id'] == article_id]
     # print(statement.at[e, 'comments'])
     iteration = iteration + 1
-    if iteration > 10000:
-        print("next 10.000")
+    if iteration > 5000:
+        print("next 5.000")
         iteration = 0
 
 print("Combined")
+end_time = datetime.now()
+print("ended at: " + str(end_time))
+print("duration: " + str(end_time - end_time))
 
 statement.to_csv(os.path.abspath("data//fullData.csv"))
 print("Done")
